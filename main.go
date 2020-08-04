2f101a7fd0b83769f16dc70dda07f91fd3ee8548
osolano1991 <oscarsolanomora11@gmail.com>
2020-08-04 16:12:46 -0600

    Update main.go

--- main.go
+++ main.go
@@ -1,17 +1,7 @@
 package main
 
-import (
-    "fmt"
-    "net/http"
-    "os"
-)
-
-func handler(writer http.ResponseWriter, request *http.Request) {
-    fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
-}
+import "fmt"
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+    fmt.Println("Hola Mundo")
 }
-//go
