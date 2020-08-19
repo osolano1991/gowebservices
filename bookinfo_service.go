47b360376b7eaf4c1b073e9ebb48f63fda7d8834
osolano1991 <oscarsolanomora11@gmail.com>
2020-08-19 17:48:38 -0600

    Update bookinfo_service.go

--- bookinfo_service.go
+++ bookinfo_service.go
@@ -1,28 +1,59 @@
 package main
 
 import (
-	"context"
-	pb "github.com/osolano1991/gowebservices/booksapp"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+    "context"
+    pb "github.com/osolano1991/gowebservices/booksapp"
+    "github.com/gofrs/uuid"
+    "google.golang.org/grpc/codes"
+    "google.golang.org/grpc/status"
 )
 
 type server struct {
-	bookMap map[string]*pb.Book
+    bookMap map[string]*pb.Book
 }
 
 func (s *server) AddBook(ctx context.Context, in *pb.Book) (*pb.BookID, error) {
-	if s.bookMap == nil {
-		s.bookMap = make(map[string]*pb.Book)
-	}
-	s.bookMap[in.Id] = in
-	return &pb.BookID{Value: in.Id}, status.New(codes.OK, "").Err()
+    //out, err := uuid.NewV4()
+    _, err := uuid.NewV4()
+    if err != nil {
+        return nil, status.Errorf(codes.Internal,
+            "Error while generating Book ID", err)
+    }
+    //in.Id = out.String()
+    if s.bookMap == nil {
+        s.bookMap = make(map[string]*pb.Book)
+    }
+    s.bookMap[in.Id] = in
+    return &pb.BookID{Value: in.Id}, status.New(codes.OK, "").Err()
 }
 
 func (s *server) GetBook(ctx context.Context, in *pb.BookID) (*pb.Book, error) {
-	value, exists := s.bookMap[in.Value]
-	if exists {
-		return value, status.New(codes.OK, "").Err()
-	}
-	return nil, status.Errorf(codes.NotFound, "Book does not exist.", in.Value)
+    value, exists := s.bookMap[in.Value]
+    if exists {
+        return value, status.New(codes.OK, "").Err()
+    }
+    return nil, status.Errorf(codes.NotFound, "Book does not exist.", in.Value)
+}
+
+func (s *server) DeleteBook(ctx context.Context, in *pb.BookID) (*pb.BookID, error) {
+    _, exists := s.bookMap[in.Value]
+    if exists {
+	delete(s.bookMap, in.Value)
+	return &pb.BookID{Value: in.Value}, status.New(codes.OK, "").Err()
+    }
+    return nil, status.Errorf(codes.NotFound, "Book does not exist.", in.Value)
+}
+
+func (s *server) UpdateBook(ctx context.Context, in *pb.Book) (*pb.BookID, error) {
+   // out, err := uuid.NewV4()
+   // if err != nil {
+   //     return nil, status.Errorf(codes.Internal,
+   //         "Error while generating Book ID", err)
+   // }
+   // in.Id = out.String()
+    if s.bookMap == nil {
+        s.bookMap = make(map[string]*pb.Book)
+    }
+    s.bookMap[in.Id] = in
+    return &pb.BookID{Value: in.Id}, status.New(codes.OK, "").Err()
 }
